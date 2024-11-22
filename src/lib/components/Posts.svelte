dec96d23397bffcd8ee4beaa9e578d7068cb9f09
blu <blu@blu.cx>
2024-11-22 18:04:05 -0500

    feat: render posts from Posts component

--- src/lib/components/Posts.svelte
+++ src/lib/components/Posts.svelte
@@ -1,5 +1,79 @@
 <script lang="ts">
-	let { posts }: { posts: MarkdownFile } = $props();
+	import { onMount } from 'svelte';
+
+	type Metadata = {
+		title: string;
+		author: string;
+		date: string; // ISO
+		description: string;
+	};
+
+	type MarkdownFileData = {
+		metadata: Metadata;
+		path: string;
+		file: MarkdownFile;
+	};
+
+	type MarkdownFiles = MarkdownFileData[];
+
+	type PathMetadata = { path: string; metadata: Metadata };
+
+	type MetaGlobImport = Record<string, any>;
+
+	let {
+		numVisible = undefined,
+		directory = undefined
+	}: {
+		numVisible: number | undefined;
+		directory: MetaGlobImport | undefined;
+	} = $props();
+
+	// Replace the relative path to the md files being passed in to the relative path
+	// of the md folder to the current component so that we can import from here
+	//
+	// - strip anything before the 'md/'
+	// - concatenate with the current relative path to the '.md' file
+
+	function makeRelativeMdPath(path: string, currentMdDir = '../') {
+		const prefixStripped = path.replace(/.*?(md\/)/, '$1');
+		return `${currentMdDir}${prefixStripped}`;
+	}
+
+	function getMdFiles(d?: MetaGlobImport): PathMetadata[] | undefined {
+		console.log({ d });
+		if (d) {
+			return Object.entries<Metadata>(d).map(([path, metadata]) => ({
+				path: makeRelativeMdPath(path),
+				metadata
+			}));
+		}
+	}
+
+	let mdFiles: MarkdownFiles | undefined = $state(undefined);
+	onMount(async () => {
+		const files = getMdFiles(directory);
+		console.log({ files });
+		if (files) {
+			console.log({ mdFiles });
+			mdFiles = await Promise.all(
+				files.map(async ({ path, metadata }): Promise<MarkdownFileData> => {
+					const file = await import(path);
+					console.log({ file });
+					return {
+						path,
+						metadata,
+						file
+					};
+				})
+			);
+		}
+	});
 </script>
 
-{JSON.stringify(posts)}
+{#if mdFiles}
+	{#each mdFiles as f}
+		<f.file.default></f.file.default>
+	{/each}
+{:else}
+	no files
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,16 @@
 <script lang="ts">
 	import Posts from '$lib/components/Posts.svelte';
 	import Quote from '$lib/components/Quote.svelte';
-	import Readme from '$lib/md/readme.md';
-	import mdFiles from '$lib/md';
-	import { metadata } from '$lib/md/readme.md';
-	import { onMount, type Component } from 'svelte';
+	import { onMount } from 'svelte';
 
 	// First value in array is filename, second is metadata
-	const articles = Object.entries(import.meta.glob('../lib/md/articles/*', { eager: true }));
-	//const blogs = import.meta.glob('../lib/md/blogs/testblog.md', { eager: true });
-	//
 
-	let md: MarkdownFile;
-	onMount(async () => {
-		const [name, metadata] = articles[1];
-		md = await import(name);
-		console.log({ name, metadata, md });
-	});
+	const rawArticles = import.meta.glob('../lib/md/articles/*', { eager: true });
 </script>
 
 <Quote></Quote>
 
-{#if md}
-	<svelte:component this={md.default} />
-{:else}
-	<p>no</p>
-{/if}
+<Posts numVisible={1} directory={rawArticles}></Posts>
 <!-- {#each Object.entries(mdFiles) as [other, f]}
 	{JSON.stringify(other)}
 	<svelte:component this={f} />
